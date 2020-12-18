5160f5e5c11f748fe07b3f80b20a543090c51206
Valerij Dobler <vd@wps.de>
2020-12-18 17:32:12 +0100

    Adds Day9 Part2

--- java/src/main/java/Day9.java
+++ java/src/main/java/Day9.java
@@ -1,28 +1,130 @@
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.buffer.CircularFifoBuffer;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
+@SuppressWarnings("unchecked")
 public class Day9 {
+    private static final String PATH = "java/input9.txt";
 
-    @SuppressWarnings("unchecked")
     public static void main(String[] args) throws IOException {
-        final String path = "java/input9.txt";
-        final CircularFifoBuffer buffer = new CircularFifoBuffer();
-        Files.lines(Paths.get(path)).mapToInt(Integer::parseInt).forEach(data -> {
-            if (buffer.size() < buffer.maxSize() || areTwoDifferentAddendsInPreamble(buffer, data))
-                buffer.add(data);
-            else {
-                log.error("Not in preamble: {}", data);
+        var result = part1();
+        part2(result);
+    }
+
+    private static void part2(int number) {
+        Collection<Long> numbers = parseInputToListOfLongs();
+        var sum = getSumOfSummedSubsequence(numbers, number);
+        assert sum == 1766397 : "correct answer";
+    }
+
+    private static List<Long> parseInputToListOfLongs() {
+        try {
+            return Files.lines(Paths.get(PATH))
+                    .map(Long::parseLong)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static int part1() throws IOException {
+        final CircularFifoBuffer buffer = new CircularFifoBuffer(25);
+        int result = -1;
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(PATH)))) {
+            for (String line; (line = br.readLine()) != null; ) {
+                final var data = Integer.parseInt(line);
+                if (buffer.size() < buffer.maxSize() || areTwoDifferentAddendsInPreamble(buffer, data))
+                    buffer.add(data);
+                else {
+                    result = data;
+                    break;
+                }
             }
-        });
+        }
+
+        assert result == 14144619 : "Correct answer";
+        log.info("Part1 result={}\n", result);
+
+        return result;
     }
 
     public static boolean areTwoDifferentAddendsInPreamble(Collection<Integer> preamble, int number) {
         return preamble.stream().anyMatch(data1 -> preamble.contains(number - data1) && data1 != number - data1);
+    }
+
+    public static long getSumOfSummedSubsequence(Collection<Long> preamble, int number) {
+        int l = 0, r = 0;
+        final int size = preamble.size();
+        while (l < size) {
+            var sum = consecutiveSum(preamble, l, r);
+            if (sum == number) {
+                var range = preamble.stream().skip(l).limit(r-l).collect(Collectors.toList());
+                var min = range.stream().min(Comparator.naturalOrder()).orElseThrow();
+                var max = range.stream().max(Comparator.naturalOrder()).orElseThrow();
+                log.info("Has sequence l={}, r={}, sum={}", min, max, min+max);
+                return min+max;
+            } else if (sum < number) {
+                // it's not in it.
+                if (r == size-1) {
+                    log.error("It's not in it!");
+                    return Long.MIN_VALUE;
+                }
+                r++;
+            } else {
+                l++;
+            }
+        }
+        return Long.MIN_VALUE;
+    }
+
+    public static boolean hasPreambleSummedSubsequence(Collection<Long> preamble, int number) {
+        int l = 0, r = 0;
+        final int size = preamble.size();
+        while (l < size) {
+            var sum = consecutiveSum(preamble, l, r);
+            if (sum == number) {
+                var range = preamble.stream().skip(l).limit(r-l).collect(Collectors.toList());
+                var min = range.stream().min(Comparator.naturalOrder()).orElseThrow();
+                var max = range.stream().max(Comparator.naturalOrder()).orElseThrow();
+                log.info("WRONG! l={}, r={}, sum={}", l, r, l + r);
+                log.info("Has sequence l={}, r={}, sum={}", min, max, min+max);
+                return true;
+            } else if (sum < number) {
+                // it's not in it.
+                if (r == size-1) {
+                    log.error("It's not in it!");
+                    return false;
+                }
+                r++;
+            } else {
+                l++;
+            }
+        }
+        return false;
+    }
+
+    static long consecutiveSum(Collection<Long> preamble, int l, int r) {
+        if (l == r)
+            return (long) preamble.toArray()[l];
+        var arr = preamble.toArray(new Long[0]);
+        long sum = arr[l];
+        while (l < r) {
+            l++;
+            if (l == arr.length) break;
+            sum += arr[l];
+        }
+        return sum;
     }
 }
--- java/src/test/java/Day9Test.java
+++ java/src/test/java/Day9Test.java
@@ -1,27 +1,32 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.*;
 import static org.hamcrest.core.Is.is;
 
 class Day9Test {
+
+    public static final long THE_ANSWER = 42;
 
     @ParameterizedTest
     @ValueSource(ints = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 20, 25, 26, 27, 49})
     void areTwoDifferentAddendsInPreamble_validData_isTrue(int i) {
         // Arrange
         final List<Integer> preamble = IntStream.rangeClosed(1, 25).boxed().collect(Collectors.toList());
-        final var day = new Day9();
 
         // Act
-        final boolean result = day.areTwoDifferentAddendsInPreamble(preamble, i);
+        final boolean result = Day9.areTwoDifferentAddendsInPreamble(preamble, i);
 
         // Assert
         assertThat(result, is(true));
@@ -32,10 +37,9 @@ class Day9Test {
     void areTwoDifferentAddendsInPreamble_invalidData_isFalse(int i) {
         // Arrange
         final List<Integer> preamble = IntStream.rangeClosed(1, 25).boxed().collect(Collectors.toList());
-        final var day = new Day9();
 
         // Act
-        final boolean result = day.areTwoDifferentAddendsInPreamble(preamble, i);
+        final boolean result = Day9.areTwoDifferentAddendsInPreamble(preamble, i);
 
         // Assert
         assertThat(result, is(false));
@@ -45,10 +49,9 @@ class Day9Test {
     public void smallExample_containsBeforePivot_returnsTrue() {
         // Arrange
         final Collection<Integer> preamble = Arrays.asList(35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150);
-        final var day = new Day9();
 
         // Act
-        final boolean result = day.areTwoDifferentAddendsInPreamble(preamble, 182);
+        final boolean result = Day9.areTwoDifferentAddendsInPreamble(preamble, 182);
 
         // Assert
         assertThat(result, is(true));
@@ -58,12 +61,133 @@ class Day9Test {
     public void smallExample_containsAtPivot_returnsFalse() {
         // Arrange
         final Collection<Integer> preamble = Arrays.asList(95, 102, 117, 150, 182);
-        final var day = new Day9();
 
         // Act
-        final boolean result = day.areTwoDifferentAddendsInPreamble(preamble, 127);
+        final boolean result = Day9.areTwoDifferentAddendsInPreamble(preamble, 127);
 
         // Assert
         assertThat(result, is(false));
+    }
+
+
+    /* Part 2 */
+    @Test
+    public void consecutiveSum_oneNumber_returnsTheNumber() {
+        // Arrange
+        final var number = 7L;
+        Collection<Long> preamble = Collections.singleton(number);
+
+        // Act
+        var result = Day9.consecutiveSum(preamble, 0, 0);
+
+        // Assert
+        assertThat(result, is(number));
+    }
+
+    @Test
+    public void consecutiveSum_twoNumbers_returnsTheirSum() {
+        // Arrange
+        Collection<Long> preamble = Arrays.asList(7L, THE_ANSWER);
+
+        // Act
+        var result = Day9.consecutiveSum(preamble, 0, 1);
+
+        // Assert
+        assertThat(result, is(7 + THE_ANSWER));
+    }
+
+    public void consecutiveSum_threeNumbers_returnsTheirSum() {
+        // Arrange
+        Collection<Long> preamble = Arrays.asList(7L, THE_ANSWER, THE_ANSWER);
+
+        // Act
+        var result = Day9.consecutiveSum(preamble, 0, 2);
+
+        // Assert
+        assertThat(result, is(7 + THE_ANSWER * 2));
+    }
+
+    public void consecutiveSum_threeNumbers_returnsTheSumOfFirstTwo() {
+        // Arrange
+        Collection<Long> preamble = Arrays.asList(7L, THE_ANSWER, THE_ANSWER);
+
+        // Act
+        var result = Day9.consecutiveSum(preamble, 0, 1);
+
+        // Assert
+        assertThat(result, is(7 + THE_ANSWER));
+    }
+
+    @Test
+    public void consecutiveSum_beforeSubSequence_returnsFalse() {
+        // Arrange
+        final Collection<Long> preamble = Arrays.asList(20L, 15L, 25L, 47L);
+
+        // Act
+        final var result = Day9.consecutiveSum(preamble, 0, 3);
+
+        // Assert
+        assertThat(result, is(107L));
+    }
+
+
+    @Test
+    public void hasSequence_fullSequence_returnsTrue() {
+        // Arrange
+        final Collection<Long> preamble = Arrays.asList(35L, 20L, 15L, 25L, 47L, 40L, 62L, 55L, 65L, 95L, 102L, 117L, 150L);
+
+        // Act
+        final boolean result = Day9.hasPreambleSummedSubsequence(preamble, 127);
+
+        // Assert
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void hasSequence_beforeSubSequence_returnsFalse() {
+        // Arrange
+        final Collection<Long> preamble = Arrays.asList(20L, 15L, 25L, 47L);
+
+        // Act
+        final boolean result = Day9.hasPreambleSummedSubsequence(preamble, 127);
+
+        // Assert
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void hasSequence_afterSubSequence_returnsFalse() {
+        // Arrange
+        final Collection<Long> preamble = Arrays.asList(25L, 47L, 40L, 62L);
+
+        // Act
+        final boolean result = Day9.hasPreambleSummedSubsequence(preamble, 127);
+
+        // Assert
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void hasSequence_exactlyAtSubsequence_returnsTrue() {
+        // Arrange
+        final Collection<Long> preamble = Arrays.asList(15L, 25L, 47L, 40L);
+
+        // Act
+        final boolean result = Day9.hasPreambleSummedSubsequence(preamble, 127);
+
+        // Assert
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void hasSequence_moreThanSubSequence_returnsTrue() {
+        // Arrange
+        final Collection<Long> preamble = Arrays.asList(20L, 15L, 25L, 47L, 40L, 62L);
+
+        // Act
+        final boolean result = Day9.hasPreambleSummedSubsequence(preamble, 127);
+
+        // Assert
+        assertThat(result, is(true));
     }
 }
