5f6c20e6757ce70e8bf7dc68785dbcf424940ca9
Valerij Dobler <vd@wps.de>
2020-12-22 22:37:36 +0100

    Day 10 part 2
    fix day 10 part 2

--- java/src/main/java/Day10.java
+++ java/src/main/java/Day10.java
@@ -1,26 +1,53 @@
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Slf4j public class Day10 {
+    static Map<Integer, Long> dp;
+
     public static void main(String[] args) {
         final var adapters = AoCUtil.parseInputToListOfInts(10);
-        assert adapters != null;
         int result1 = part1(adapters);
         assert result1 == 2176 : "Correct answer";
         log.info("Part1 solution={}", result1);
+
+        long result2 = part2(adapters);
+        assert result2 == 18512297918464L : "Correct answer";
+        log.info("Part2 solution={}", result2);
     }
 
     public static int part1(List<Integer> adapters) {
-        final var changes = Arrays.asList(0, 0, 0, 0);
+        final var changes = new int[]{ 0, 0, 0, 0};
         adapters.stream().sorted().reduce((a, b) -> {
-            changes.set(b - a, changes.get(b - a) + 1);
+            changes[b - a]++;
             return b;
         });
-        log.debug("Changes={}",changes);
-
-        return (1 + changes.get(1)) * (1 + changes.get(3));
+        return (1 + changes[1]) * (1 + changes[3]);
     }
+
+    public static long part2(List<Integer> adapters) {
+        dp = new HashMap<>();
+        adapters = new ArrayList<>(adapters);
+        adapters.sort(Comparator.naturalOrder());
+        adapters.add(0, 0);
+        adapters.add(adapters.get(adapters.size() - 1) + 3);
+        return doSolve(adapters, 0);
+    }
+
+    private static long doSolve(List<Integer> xs, int i) {
+        if (i == xs.size() - 1) return 1;
+        if (dp.containsKey(i)) return dp.get(i);
+        long ans = 0;
+        for (int j = i + 1; j < xs.size(); ++j) {
+            if (xs.get(j) - xs.get(i) <= 3) ans += doSolve(xs, j);
+        }
+        dp.put(i, ans);
+        return ans;
+    }
+
 }
--- java/src/test/java/Day10Test.java
+++ java/src/test/java/Day10Test.java
@@ -1,7 +1,10 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
 
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -40,6 +43,63 @@ class Day10Test {
 
         // Assert
         assertThat(result, is(220));
+    }
+
+
+    @Test public void recursiveSolve() {
+        // Arrange
+        var adapters = Arrays.asList(1, 3, 6, 7, 10);
+
+        // Act
+        final var result = Day10.part2(adapters);
+
+        // Assert
+        assertThat(result, is(2L));
+    }
+
+    @Test public void recursiveSolve2() {
+        // Arrange
+        var adapters = Arrays.asList(1, 2, 3, 4);
+
+        // Act
+        final var result = Day10.part2(adapters);
+
+        // Assert
+        assertThat(result, is(7L));
+    }
+
+    @Test public void recursiveSolveNeg() {
+        // Arrange
+        var adapters = Arrays.asList(1, 6, 7, 10);
+
+        // Act
+        final var result = Day10.part2(adapters);
+
+        // Assert
+        assertThat(result, is(0L));
+    }
+
+    @Test public void recursiveSolve_small() {
+        // Arrange
+        var adapters = Arrays.asList(16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4);
+
+        // Act
+        final var result = Day10.part2(adapters);
+
+        // Assert
+        assertThat(result, is(8L));
+    }
+
+    @Test public void recursiveSolve_big() {
+        // Arrange
+        var adapters = Arrays.asList(28, 33, 18, 42, 31, 14, 46, 20, 48, 47,
+                        24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3);
+
+        // Act
+        final var result = Day10.part2(adapters);
+
+        // Assert
+        assertThat(result, is(19_208L));
     }
 
 }
