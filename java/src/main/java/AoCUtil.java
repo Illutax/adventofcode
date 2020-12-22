950ae615aeb2a3e56b07ec0a3f56c86a10b04639
Valerij Dobler <vd@wps.de>
2020-12-22 22:32:30 +0100

    Util methods returns never null

--- java/src/main/java/AoCUtil.java
+++ java/src/main/java/AoCUtil.java
@@ -1,28 +1,29 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class AoCUtil {
-    public static List<Long> parseInputToListOfLongs(int day) {
+    @org.jetbrains.annotations.NotNull public static List<Long> parseInputToListOfLongs(int day) {
         try {
             return Files.lines(Paths.get("input" + day + ".txt"))
                     .map(Long::parseLong)
                     .collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
+            return Collections.emptyList();
         }
     }
-    public static List<Integer> parseInputToListOfInts(int day) {
+    @org.jetbrains.annotations.NotNull public static List<Integer> parseInputToListOfInts(int day) {
         try {
             return Files.lines(Paths.get("input" + day + ".txt"))
                     .map(Integer::parseInt)
                     .collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
+            return Collections.emptyList();
         }
     }
 }
