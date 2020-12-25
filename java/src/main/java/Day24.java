8f76e9066eda74aa7f505872060dbc7adfc6d26e
Valerij Dobler <vd@wps.de>
2020-12-25 23:31:41 +0100

    Day 24 Part 2

--- java/src/main/java/Day24.java
+++ java/src/main/java/Day24.java
@@ -1,15 +1,19 @@
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 
 enum Direction {
-    EAST(1, 0), SOUTH_EAST(0, 1), SOUTH_WEST(-1, 1), WEST(-1, 0), NORTH_WEST(0,
-                    -1), NORTH_EAST(1, -1);
+    EAST(1, 0),
+    SOUTH_EAST(0, 1), SOUTH_WEST(-1, 1),
+    WEST(-1, 0),
+    NORTH_WEST(0, -1), NORTH_EAST(1, -1);
 
     private final Point _dir;
 
@@ -22,44 +26,58 @@ enum Direction {
     }
 }
 
-
-class Point extends java.awt.Point {
-
-    public static final Point ORIGIN = new Point(0, 0);
-
-    public Point(@NotNull java.awt.Point p) {
-        super(p);
-    }
-
-    public Point(int x, int y) {
-        super(x, y);
-    }
-
-    public void translate(Point dir) {
-        super.translate(dir.x, dir.y);
-    }
-}
-
-
 @Slf4j public class Day24 {
+    private static final boolean BLACK_SIDE_UP = true;
+    private static final boolean WHITE_SIDE_UP = false;
+
+    private static long countBlackTiles(Map<Point, Boolean> tiles) {
+        return tiles.values().stream()
+                        .filter(b -> b)
+                        .count();
+    }
+
+    public static long countAdjestBlackTiles(Map<Point, Boolean> flippedTiles, Point position) {
+        return Arrays.stream(Direction.values())
+                        .filter(dir -> getNeighbourAt(flippedTiles, position, dir.getDirection()))
+                        .count();
+    }
+
     public static void main(String[] args) {
         var lines = AoCUtil.parseInputToListStrings(24);
         var result1 = part1(lines);
-        assert result1 == 266 : "correct answer";
         log.info("Part 1 = {}", result1);
+
+        var result2 = part2(lines);
+        log.info("Part 2 = {}", result2);
     }
 
     public static long part1(List<String> lines) {
+        var flippedTiles = initialFloorTiling(lines);
+        return countBlackTiles(flippedTiles);
+    }
+
+    @NotNull private static Map<Point, Boolean> initialFloorTiling(
+                    List<String> lines) {
         Map<Point, Boolean> flippedTiles = new HashMap<>();
         for (var line : lines) {
-            final List<Direction> directions = getDirections(line);
-            final Point destination = walkDirections(directions);
-            flippedTiles.put(destination,
-                            !flippedTiles.getOrDefault(destination, false));
+            final var directions = getDirections(line);
+            final var destination = walkDirections(directions);
+            final var isBlackSideUp = flippedTiles
+                            .getOrDefault(destination, false);
+            flippedTiles.put(destination, !isBlackSideUp);
+            addNewWhiteNeighbours(flippedTiles, destination);
         }
-        return flippedTiles.values().stream()
-                        .filter(b -> b)
-                        .count();
+        return flippedTiles;
+    }
+
+    private static void addNewWhiteNeighbours(Map<Point, Boolean> flippedTiles, Point target) {
+        for (Direction dir : Direction.values()) {
+            final var neighbour = new Point(
+                            target.x + dir.getDirection().x,
+                            target.y + dir.getDirection().y);
+            if (!flippedTiles.containsKey(neighbour))
+                flippedTiles.put(neighbour, false);
+        }
     }
 
     public static List<Direction> getDirections(String line) {
@@ -85,16 +103,47 @@ class Point extends java.awt.Point {
             } else if (line.charAt(i) == 'w') {
                 directions.add(Direction.WEST);
                 i++;
+            } else {
+                throw new RuntimeException("Invalid input! " + line);
             }
         }
         return directions;
     }
 
     public static Point walkDirections(List<Direction> directions) {
-        var position = new Point(0, 0);
-        for (var dir : directions) {
-            position.translate(dir.getDirection());
+        var destination = new Point(0, 0);
+        for (var dir : directions)
+            destination.translate(dir.getDirection().x, dir.getDirection().y);
+        return destination;
+    }
+
+    public static long part2(List<String> lines) {
+        var flippedTiles = initialFloorTiling(lines);
+        for (int daysPassed = 0; daysPassed < 100; ++daysPassed)
+            permuteExhibition(flippedTiles);
+        return countBlackTiles(flippedTiles);
+    }
+
+    public static void permuteExhibition(Map<Point, Boolean> tiles) {
+        var oldTiling = new HashMap<>(tiles);
+        for (var point : oldTiling.keySet()) {
+            var amountOfBlackTiledNeighbours = countAdjestBlackTiles(oldTiling,
+                            point);
+            if (oldTiling.get(point) == BLACK_SIDE_UP &&
+                            (amountOfBlackTiledNeighbours == 0 || amountOfBlackTiledNeighbours > 2)) {
+                tiles.put(point, WHITE_SIDE_UP);
+                addNewWhiteNeighbours(tiles, point);
+            } else if (oldTiling.get(point) == WHITE_SIDE_UP && amountOfBlackTiledNeighbours == 2) {
+                tiles.put(point, BLACK_SIDE_UP);
+                addNewWhiteNeighbours(tiles, point);
+            }
         }
-        return position;
+    }
+
+    private static boolean getNeighbourAt(Map<Point, Boolean> flippedTiles,
+                                          Point target, Point dir) {
+        return flippedTiles.getOrDefault(
+                        new Point(target.x + dir.x, target.y + dir.y),
+                        false);
     }
 }
--- java/src/test/java/Day24Test.java
+++ java/src/test/java/Day24Test.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,6 +20,28 @@ class Day24Test {
 
         // Assert
         assertThat(amountOfFlippedTiles, is(266L));
+    }
+
+    @Test void part1_sample() {
+        // Arrange
+        var lines = AoCUtil.parseInputToListStrings(24_0);
+
+        // Act
+        final long amountOfFlippedTiles = Day24.part1(lines);
+
+        // Assert
+        assertThat(amountOfFlippedTiles, is(10L));
+    }
+
+    @Test void part2_sample() {
+        // Arrange
+        var lines = AoCUtil.parseInputToListStrings(24_0);
+
+        // Act
+        final long amountOfFlippedTiles = Day24.part2(lines);
+
+        // Assert
+        assertThat(amountOfFlippedTiles, is(2208L));
     }
 
     @Test void getDirections_esew() {
@@ -65,6 +90,75 @@ class Day24Test {
         final var result = Day24.walkDirections(directionsToWalk);
 
         // Assert
-        assertThat(result, is(Point.ORIGIN));
+        assertThat(result, is(new Point(0, 0)));
+    }
+
+    @Test void countAdjestBlackTiles() {
+        // Arrange
+        final Map<Point, Boolean> tiles = Map.of(
+                        Direction.EAST.getDirection(), Boolean.TRUE,
+                        Direction.WEST.getDirection(), Boolean.TRUE,
+                        Direction.NORTH_EAST.getDirection(), Boolean.TRUE,
+                        Direction.NORTH_WEST.getDirection(), Boolean.TRUE,
+                        Direction.SOUTH_EAST.getDirection(), Boolean.TRUE,
+                        Direction.SOUTH_WEST.getDirection(), Boolean.TRUE
+                        );
+
+        // Act
+        final var result = Day24.countAdjestBlackTiles(tiles, new Point(0, 0));
+
+        // Assert
+        assertThat(result, is(6L));
+    }
+
+    @Test void permuteExhibition_OneBlackEntry() {
+        // Arrange
+        final Map<Point, Boolean> initialTiles = new HashMap<>();
+        final Point pointOfInterest = new Point(0, 0);
+        initialTiles.put(pointOfInterest, Boolean.TRUE);
+
+        // Act
+        Day24.permuteExhibition(initialTiles);
+
+        // Assert
+        assertThat(initialTiles.size(), is(7));
+        assertThat(initialTiles.get(pointOfInterest), is(false));
+    }
+
+    @Test void permuteExhibition_OneWhiteEntry_StaysUnflipped() {
+        // Arrange
+        final Map<Point, Boolean> initialTiles = new HashMap<>();
+        initialTiles.put(new Point(0, 0), Boolean.FALSE);
+        final Map<Point, Boolean> expectedTiles = new HashMap<>();
+        expectedTiles.put(new Point(0, 0), Boolean.FALSE);
+
+        // Act
+        Day24.permuteExhibition(initialTiles);
+
+        // Assert
+        assertThat(initialTiles.size(), is(1));
+        assertThat(initialTiles, is(expectedTiles));
+    }
+
+    @Test void permuteExhibition_OneWhiteEntryWithTwoBlackNeighbours() {
+        // Arrange
+        final Map<Point, Boolean> initialTiles = new HashMap<>(){{
+            put(new Point(0, 0), Boolean.FALSE);
+            put(new Point(1, 0), Boolean.TRUE);
+            put(new Point(-1, 0), Boolean.TRUE);
+        }};
+        final Map<Point, Boolean> expectedTiles = new HashMap<>(){{
+            put(new Point(0, 0), Boolean.TRUE);
+            put(new Point(1, 0), Boolean.FALSE);
+            put(new Point(-1, 0), Boolean.FALSE);
+        }};
+
+        // Act
+        Day24.permuteExhibition(initialTiles);
+
+        // Assert
+        assertThat(initialTiles.size(), is(13));
+        for( var point : expectedTiles.keySet())
+            assertThat(initialTiles.get(point), is(expectedTiles.get(point)));
     }
 }
