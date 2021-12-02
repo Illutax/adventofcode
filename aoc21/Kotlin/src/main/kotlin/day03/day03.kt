690a40ab5ccf05354e9f09df8105cee77559f895
Valerij Dobler <vd@wps.de>
2021-12-02 15:33:58 +0100

    Add Kotlin template

---
+++ aoc21/Kotlin/src/main/kotlin/day03/day03.kt
@@ -0,0 +1,22 @@
+package day03
+
+import readInput
+
+typealias Input = List<Int>
+typealias Output = Int
+
+fun main() {
+    val input = readInput("3", ::mapInput)
+    println("part1: ${template.part1(input)}")
+    println("part2: ${template.part2(input)}")
+}
+
+fun mapInput(lines: Sequence<String>): Input = TODO()
+
+fun part1(input: Input): Output {
+    TODO()
+}
+
+fun part2(input: Input): Output {
+    TODO()
+}
---
+++ aoc21/Kotlin/src/test/kotlin/day03/Day03Test.kt
@@ -0,0 +1,31 @@
+package day03
+
+import org.junit.jupiter.api.Test
+import readInput
+
+class Day03Test {
+
+    private val path = "xx_test"
+
+    @Test
+    fun `parse input`()
+    {
+        TODO()
+    }
+
+    @Test
+    fun `part 1 example`()
+    {
+        val input = readInput(path, ::mapInput);
+        //assertEquals(Int.MIN_VALUE, part1(input))
+        TODO()
+    }
+
+    @Test
+    fun `part 2 example`()
+    {
+        val input = readInput(path, ::mapInput);
+        //assertEquals(Int.MIN_VALUE, part2(input))
+        TODO()
+    }
+}
