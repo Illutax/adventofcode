93524acb2a0a90b97da0678e70a78e39a86e6897
Valerij Dobler <vd@wps.de>
2021-12-02 22:43:44 +0100

    Adds overwrite and error checks

--- aoc21/input/get_input.sh
+++ aoc21/input/get_input.sh
@@ -6,7 +6,21 @@ if [[ $1 -lt 10 ]]; then D=0$1; else D=$1; fi
 
 FILEPATH=day$D.txt
 
+# check file
+if [[ -f $FILEPATH ]]; then
+    echo "$FILEPATH exists."
+    exit 1
+fi
 
-echo getting input for day $1...
-#curl -b $(cat .session) https://adventofcode.com/$YEAR/day/$1/input --silent > $FILEPATH
-echo "wrote input to $FILEPATH with $(cat $FILEPATH | wc -l) lines"
+echo "getting input for day $1..."
+curl -b $(cat .session) https://adventofcode.com/$YEAR/day/$1/input --silent > $FILEPATH
+
+L=$(cat $FILEPATH | wc -l)
+if [[ $L -eq 1 ]]; then
+    echo "ERROR:"
+    echo $(cat $FILEPATH)
+    rm $FILEPATH
+    exit 1
+fi
+
+echo "wrote input to $FILEPATH with $L lines"
