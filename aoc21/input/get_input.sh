eb477ebdd2fdd238f2f5a24807ae3bd50b781cfd
Valerij Dobler <vd@wps.de>
2021-12-02 23:12:21 +0100

    Adds range support for script

--- aoc21/input/get_input.sh
+++ aoc21/input/get_input.sh
@@ -1,26 +1,52 @@
 #!/bin/bash
 YEAR=2021
+INPUT=$1
 
-# prefix $D with 0
-if [[ $1 -lt 10 ]]; then D=0$1; else D=$1; fi
+function download {
+    # prefix $D with 0
+    if [[ $1 -lt 10 ]]; then D=0$1; else D=$1; fi
 
-FILEPATH=day$D.txt
+    FILEPATH=day$D.txt
 
-# check file
-if [[ -f $FILEPATH ]]; then
-    echo "$FILEPATH exists."
-    exit 1
+    # check file
+    if [[ -f $FILEPATH ]]; then
+        echo "$FILEPATH exists."
+        exit 1
+    fi
+
+    echo "getting input for day $1..."
+    curl -b $(cat .session) https://adventofcode.com/$YEAR/day/$1/input --silent > $FILEPATH
+
+    L=$(cat $FILEPATH | wc -l)
+    if [[ $L -eq 1 ]]; then
+        echo "ERROR:"
+        echo $(cat $FILEPATH)
+        rm $FILEPATH
+        exit 1
+    fi
+
+    echo "wrote input to $FILEPATH with $L lines"
+}
+
+# download a range of inputs
+if [[ $INPUT == *".."* ]]; then
+    RANGE=(${INPUT//../ })
+    START=${RANGE[0]}
+    END=${RANGE[1]}
+    if [[ $START > $END ]]; then
+        echo "Invalid range $START to $END"
+        exit 1
+    else
+    echo "NICE! downlaoding for $START to $END"
+        i=$START
+        j=$END
+        while [ $i -le $j ] 
+        do
+            download $i
+            i=$((i + 1));
+        done
+        exit 0
+    fi
 fi
 
-echo "getting input for day $1..."
-curl -b $(cat .session) https://adventofcode.com/$YEAR/day/$1/input --silent > $FILEPATH
-
-L=$(cat $FILEPATH | wc -l)
-if [[ $L -eq 1 ]]; then
-    echo "ERROR:"
-    echo $(cat $FILEPATH)
-    rm $FILEPATH
-    exit 1
-fi
-
-echo "wrote input to $FILEPATH with $L lines"
+download $INPUT
