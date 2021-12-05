790df2f46a00a6b0f09bd96603d25e263ded1196
Valerij Dobler <vd@wps.de>
2021-12-05 22:13:56 +0100

    Adds Kotlin day04

--- aoc21/Kotlin/src/main/kotlin/day04/Day04.kt
+++ aoc21/Kotlin/src/main/kotlin/day04/Day04.kt
@@ -2,21 +2,117 @@ package day04
 
 import readInput
 
-typealias Input = List<Int>
+typealias Input = BingoGame
 typealias Output = Int
 
 fun main() {
-    val input = readInput("01", ::mapInput)
-    println("part1: ${part1(input)}")
-    println("part2: ${part2(input)}")
+    val game = readInput("04", ::mapInput)
+    println("Amount of boards: ${game.boards().count()}")
+    part2(game) // part 1 & 2
 }
 
-fun mapInput(lines: Sequence<String>): Input = TODO()
+class Board(lines: List<String>) {
+    private val numbers: List<Int>
+    private val markedNumbers: MutableMap<Int, Boolean>
 
-fun part1(input: Input): Output {
-    TODO()
+    init {
+        this.numbers = lines.flatMap { it.split(regex = "\\s+".toRegex()).filter(String::isNotEmpty).map(String::toInt) }
+        markedNumbers = HashMap()
+    }
+
+    fun unmarkedNumbers(): List<Int> = numbers.filter { number -> !markedNumbers.contains(number) }
+
+    fun addNumber(number: Int) {
+        if (numbers.contains(number)) markedNumbers[number] = true
+    }
+
+    fun checkWinCondition(): Boolean = checkRows() || checkColumns()
+
+    private fun checkRows(): Boolean = numbers.asSequence().chunked(5).any { row -> row.all { cell -> markedNumbers.contains((cell)) } }
+
+    private fun checkColumns(): Boolean = IntProgression.fromClosedRange(0, 4, 1)
+        .any { x ->
+            IntProgression.fromClosedRange(0, 4, 1)
+                .all { y -> markedNumbers.contains(pointToNumber(x, y)) }
+        }
+
+    private fun pointToNumber(x: Int, y: Int): Int = numbers[x + y * 5]
+    internal fun numbers(): List<Int> = numbers.toList()
+    override fun toString(): String = "Board(numbers=$numbers, checkedNumbers=\n${printCheckedNumbers()})"
+
+    private fun printCheckedNumbers(): String {
+        val sb = StringBuilder()
+        for (y in 0..4) {
+            for (x in 0..4)
+                sb.append(
+                    if (markedNumbers.contains(pointToNumber(x, y))) String.format("[%1$2s]", numbers[x + y * 5]) else String.format(
+                        "%1$3s ",
+                        numbers[x + y * 5]
+                    )
+                )
+            sb.append('\n')
+        }
+        return sb.toString()
+    }
 }
 
-fun part2(input: Input): Output {
-    TODO()
+class BingoGame(numbers: String = "", boards: List<List<String>> = listOf()) {
+    private val numbers: List<Int>
+    private val boards: List<Board>
+
+    init {
+        this.numbers = numbers.split(",").map(String::toInt)
+        this.boards = boards.map { Board(it.drop(1)) }
+    }
+
+    fun numbers(): Iterable<Int> = numbers.asIterable()
+    fun boards(): List<Board> = boards.toList()
+    override fun toString(): String = "BingoGame(numbers=$numbers, boards=$boards)"
+}
+
+fun mapInput(lines: Sequence<String>): Input {
+    val trimmed = lines.map(String::trim).toList()
+    return BingoGame(trimmed.first(), trimmed.drop(1).chunked(6))
+}
+
+internal fun part1(game: Input): Output {
+    for (number in game.numbers()) {
+        var winningBoard: Board? = null
+        val winningBoards: MutableList<Board> = ArrayList()
+        for (board in game.boards()) {
+            board.addNumber(number)
+
+            if (board.checkWinCondition()) {
+                winningBoard = board
+                winningBoards.add(board)
+            }
+        }
+        if (winningBoard != null) {
+            println("$number; ${winningBoard.unmarkedNumbers().sum()}; ${winningBoard.unmarkedNumbers().sum() * number}; $winningBoard")
+            return number * winningBoard.unmarkedNumbers().sum()
+        }
+    }
+    throw IllegalStateException("Should never happen")
+}
+
+fun part2(game: Input): Output {
+    val winningBoards: MutableList<Board> = ArrayList()
+    val winningNumbers: MutableList<Int> = ArrayList();
+    for (number in game.numbers()) {
+        for (board in game.boards()) {
+            if (!winningBoards.contains(board)) {
+                board.addNumber(number)
+                if (board.checkWinCondition()) {
+                    winningBoards.add(board)
+                    winningNumbers.add(number)
+                }
+            }
+        }
+    }
+    assert(winningBoards.count() == winningNumbers.count());
+
+    println("Part1: ${winningNumbers.first() * winningBoards.first().unmarkedNumbers().sum()}")
+    println("Part2: ${winningNumbers.last() * winningBoards.last().unmarkedNumbers().sum()}")
+
+    return winningNumbers.last() * winningBoards.last().unmarkedNumbers().sum()
 }
--- aoc21/Kotlin/src/test/kotlin/day04/Day04Test.kt
+++ aoc21/Kotlin/src/test/kotlin/day04/Day04Test.kt
@@ -1,36 +1,46 @@
 package day04
 
 import org.junit.jupiter.api.Test
+import readInput
 import kotlin.test.assertEquals
 
 class Day04Test {
 
-    private val path = "xx_test"
-    private val exampleInput = """"""
+    private val path = "04_test"
 
     @Test
-    fun `parse input`()
-    {
-        val input = mapInput(exampleInput.lineSequence());
-        assertEquals(null as Output, part1(input))
-        TODO()
+    fun `parse input`() {
+        // Arrange
+        val expectedNumbers = listOf(7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1)
+        val expectedBoards = listOf(
+            listOf(22, 13, 17, 11, 0, 8, 2, 23, 4, 24, 21, 9, 14, 16, 7, 6, 10, 3, 18, 5, 1, 12, 20, 15, 19),
+            listOf(3, 15, 0, 2, 22, 9, 18, 13, 17, 5, 19, 8, 7, 25, 23, 20, 11, 10, 24, 4, 14, 21, 16, 12, 6),
+            listOf(14, 21, 17, 24, 4, 10, 16, 15, 9, 19, 18, 8, 23, 26, 20, 22, 11, 13, 6, 5, 2, 0, 12, 3, 7)
+        )
+
+        // Act
+        val input = readInput(path, ::mapInput);
+
+        // Assert
+        assertEquals(listOf(25, 25, 25), expectedBoards.map { it.count() }) // sanity check
+
+        assertEquals(expectedNumbers, input.numbers())
+        val actualNumbers = input.boards().map { it.numbers() }
+        assertEquals(listOf(25, 25, 25), actualNumbers.map { it.count() })
+        assertEquals(expectedBoards, actualNumbers)
     }
 
     @Test
     fun `part 1 example`()
     {
-        val input = mapInput(exampleInput.lineSequence())
-//        val input = readInput(path, ::mapInput);
-        assertEquals(Int.MIN_VALUE, part1(input))
-        TODO()
+        val input = readInput(path, ::mapInput);
+        assertEquals(4512, part1(input))
     }
 
     @Test
     fun `part 2 example`()
     {
-        val input = mapInput(exampleInput.lineSequence())
-//        val input = readInput(path, ::mapInput);
-        assertEquals(Int.MIN_VALUE, part2(input))
-        TODO()
+        val input = readInput(path, ::mapInput);
+        assertEquals(1924, part2(input))
     }
 }
