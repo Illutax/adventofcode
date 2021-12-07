97042fd57e10b145b41fea07feefa979c295d484
Valerij Dobler <vd@wps.de>
2021-12-07 21:17:02 +0100

    Add Kotlin day06 naïve part1

--- aoc21/Kotlin/src/main/kotlin/day06/day06.kt
+++ aoc21/Kotlin/src/main/kotlin/day06/day06.kt
@@ -6,17 +6,25 @@ typealias Input = List<Int>
 typealias Output = Int
 
 fun main() {
-    val input = readInput("01", ::mapInput)
+    val input = readInput("06", ::mapInput)
+    println("Amount of lantern fish: ${input.count()}")
+
     println("part1: ${part1(input)}")
     println("part2: ${part2(input)}")
 }
 
-fun mapInput(lines: Sequence<String>): Input = TODO()
+fun mapInput(lines: Sequence<String>): Input = lines.flatMap { it.split(",").map(String::toInt) }.toList()
 
-fun part1(input: Input): Output {
-    TODO()
+fun part1(input: Input): Output = tickNTimes(input, 80).count()
+
+fun tickNTimes(input: Input, times: Int): Input {
+    var newInput = input
+    repeat(times) { tick(newInput).also { newInput = it } }
+    return newInput
 }
 
-fun part2(input: Input): Output {
-    TODO()
-}
+fun tick(input: List<Int>): List<Int> = input.map { it - 1 }.flatMap { if (it < 0) listOf(6, 8) else listOf(it) }//.sortedWith(::is8)
+
+fun is8(o1: Int, o2: Int) = if (o1 == 8) 1 else if (o2 == 8) -1 else 0
+
+fun part2(input: Input): Long = TODO()
--- aoc21/Kotlin/src/test/kotlin/day06/Day06Test.kt
+++ aoc21/Kotlin/src/test/kotlin/day06/Day06Test.kt
@@ -1,36 +1,45 @@
 package day06
 
 import org.junit.jupiter.api.Test
+import readInput
 import kotlin.test.assertEquals
 
 class Day06Test {
 
-    private val path = "xx_test"
-    private val exampleInput = """"""
+    private val path = "06_test"
 
     @Test
-    fun `parse input`()
-    {
-        val input = mapInput(exampleInput.lineSequence());
-        assertEquals(null as Output, part1(input))
-        TODO()
+    fun `parse input`() {
+        val input = readInput(path, ::mapInput);
+        assertEquals(listOf(3, 4, 3, 1, 2), input)
     }
 
     @Test
-    fun `part 1 example`()
-    {
-        val input = mapInput(exampleInput.lineSequence())
-//        val input = readInput(path, ::mapInput);
-        assertEquals(Int.MIN_VALUE, part1(input))
-        TODO()
+    fun `tick once`() {
+        assertEquals(listOf(2, 3, 2, 0, 1), tick(listOf(3, 4, 3, 1, 2)))
     }
 
     @Test
-    fun `part 2 example`()
-    {
-        val input = mapInput(exampleInput.lineSequence())
-//        val input = readInput(path, ::mapInput);
-        assertEquals(Int.MIN_VALUE, part2(input))
-        TODO()
+    fun `tick 18 times`() {
+        val input = readInput(path, ::mapInput);
+        assertEquals(26, tickNTimes(input, 18).count())
+    }
+
+    @Test
+    fun `amount of fish after 18 nticks`() {
+        val input = readInput(path, ::mapInput);
+        assertEquals(26, tickNTimes(input, 18).count())
+    }
+
+    @Test
+    fun `part 1 example`() {
+        val input = readInput(path, ::mapInput);
+        assertEquals(5934, part1(input))
+    }
+
+    @Test
+    fun `part 2 example`() {
+        val input = readInput(path, ::mapInput);
+        assertEquals(26_984_457_539, part2(input))
     }
 }
