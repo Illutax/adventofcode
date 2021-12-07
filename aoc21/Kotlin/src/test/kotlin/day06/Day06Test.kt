f7791dbdf2a0cf91ccdb2e74b047548104815254
Valerij Dobler <vd@wps.de>
2021-12-07 12:53:35 +0100

    Add Kotlin day06 template

---
+++ aoc21/Kotlin/src/test/kotlin/day06/Day06Test.kt
@@ -0,0 +1,36 @@
+package day06
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day06Test {
+
+    private val path = "xx_test"
+    private val exampleInput = """"""
+
+    @Test
+    fun `parse input`()
+    {
+        val input = mapInput(exampleInput.lineSequence());
+        assertEquals(null as Output, part1(input))
+        TODO()
+    }
+
+    @Test
+    fun `part 1 example`()
+    {
+        val input = mapInput(exampleInput.lineSequence())
+//        val input = readInput(path, ::mapInput);
+        assertEquals(Int.MIN_VALUE, part1(input))
+        TODO()
+    }
+
+    @Test
+    fun `part 2 example`()
+    {
+        val input = mapInput(exampleInput.lineSequence())
+//        val input = readInput(path, ::mapInput);
+        assertEquals(Int.MIN_VALUE, part2(input))
+        TODO()
+    }
+}
