175e5bf77d13e7a8b870efe708fb2864795d5356
Valerij Dobler <vd@wps.de>
2021-12-08 07:45:08 +0100

    Add Kotlin day07 template

---
+++ aoc21/Kotlin/src/main/kotlin/day07/day07.kt
@@ -0,0 +1,22 @@
+package day07
+
+import readInput
+
+typealias Input = List<Int>
+typealias Output = Int
+
+fun main() {
+    val input = readInput("01", ::mapInput)
+    println("part1: ${day06.part1(input)}")
+    println("part2: ${day06.part2(input)}")
+}
+
+fun mapInput(lines: Sequence<String>): Input = TODO()
+
+fun part1(input: Input): Output {
+    TODO()
+}
+
+fun part2(input: Input): Output {
+    TODO()
+}
