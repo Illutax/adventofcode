b94b6adcad44b11abb5ac53c77fa2c5557883269
Valerij Dobler <vd@wps.de>
2021-12-09 09:42:52 +0100

    Add Kotlin day09 Part1

--- aoc21/Kotlin/src/main/kotlin/day09/day09.kt
+++ aoc21/Kotlin/src/main/kotlin/day09/day09.kt
@@ -1,18 +1,55 @@
 package day09
 
 import util.advent
+import java.util.*
 
-typealias Input = List<Int>
+typealias Input = List<List<Int>>
 typealias Output = Int
 
-fun main() = advent(0, ::mapInput, ::part1, ::part2)
+fun main() = advent(9, ::mapInput, ::part1, ::part2)
 
-fun mapInput(lines: Sequence<String>): Input = TODO()
+fun mapInput(lines: Sequence<String>): Input = lines.map { it.split("").filter { s -> s != "" }.map(String::toInt) }.toList()
 
-fun part1(input: Input): Output {
-    TODO()
+fun part1(grid: Input): Output {
+    val maxX = grid[0].count() - 1
+    val maxY = grid.count() - 1
+    val localMinima = LinkedList<Int>()
+    for (y in 0..maxY) {
+        for (x in 0..maxX) {
+            val cellValue = grid[y][x]
+            if (lowestNeighbours(grid, x, y, maxX, maxY) == Pair(x,y)) {
+                localMinima.add(cellValue + 1)
+                print(".")
+            }
+            else
+                print(cellValue)
+        }
+        println()
+    }
+    return localMinima.sum();
 }
 
-fun part2(input: Input): Output {
+
+@Suppress("unused")
+fun neighbours(grid: List<List<Int>>, x: Int, y: Int, maxX: Int, maxY: Int): Set<Pair<Pair<Int, Int>, Int>> {
+    val neighbours = HashSet<Pair<Pair<Int, Int>, Int>>()
+    for (j in y - 1..y + 1)
+        for (i in x - 1..x + 1)
+            if (j in 0..maxY && i in 0..maxX)
+                neighbours.add(Pair(Pair(j, i), grid[j][i]))
+    return neighbours;
+}
+
+fun lowestNeighbours(grid: List<List<Int>>, x: Int, y: Int, maxX: Int, maxY: Int): Pair<Int, Int>? {
+    val neighbours = HashSet<Pair<Pair<Int, Int>, Int>>()
+    for (j in y - 1..y + 1)
+        for (i in x - 1..x + 1)
+            if (j in 0..maxY && i in 0..maxX && (j == y || i == x))
+                neighbours.add(Pair(Pair(i, j), grid[j][i]))
+    val (lowest, secondLowest) = neighbours.sortedBy { it.second }.take(2)
+    return if (lowest.second == secondLowest.second) null else lowest.first
+}
+
+fun part2(input: Input): Any {
     TODO()
 }
--- aoc21/Kotlin/src/test/kotlin/day09/Day09Test.kt
+++ aoc21/Kotlin/src/test/kotlin/day09/Day09Test.kt
@@ -6,26 +6,32 @@ import kotlin.test.assertEquals
 
 class Day09Test {
 
-    private val path = "_test"
+    private val path = "09_test"
 
     @Test
     fun `parse input`()
     {
         val input = readInput(path, ::mapInput);
-        assertEquals(listOf(), input)
+        assertEquals(
+            listOf(
+                listOf(2, 1, 9, 9, 9, 4, 3, 2, 1, 0),
+                listOf(3, 9, 8, 7, 8, 9, 4, 9, 2, 1),
+                listOf(9, 8, 5, 6, 7, 8, 9, 8, 9, 2),
+                listOf(8, 7, 6, 7, 8, 9, 6, 7, 8, 9),
+                listOf(9, 8, 9, 9, 9, 6, 5, 6, 7, 8)), input)
     }
 
     @Test
     fun `part 1 example`()
     {
         val input = readInput(path, ::mapInput);
-        assertEquals(Int.MIN_VALUE, part1(input))
+        assertEquals(15, part1(input))
     }
 
     @Test
     fun `part 2 example`()
     {
         val input = readInput(path, ::mapInput);
-        assertEquals(Int.MIN_VALUE, part2(input))
+        assertEquals(1134, part2(input))
     }
 }
